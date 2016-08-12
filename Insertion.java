0abfb71f13a2ab76a05d6a643f411fb10be70f6a
YYSU <jim123820@yahoo.com.tw>
2016-08-12 10:47:09 +0800

    add Insertion sort

---
+++ Insertion.java
@@ -0,0 +1,32 @@
+public class Insertion {
+    public static void main(String[] argv) {
+        int[] array = { -10000000, 5, 3, 4, 6, 8, 10, 1};
+        //排序前
+        System.out.println("Before sort");
+        print(array);
+        System.out.println();
+
+        Insort(array, array.length);
+        //排序後
+        System.out.println("After sort");
+        print(array);
+    }
+    public static void Insert(int[] array, int r, int index) {
+        while(r < array[index]) {
+            array[index + 1] = array[index];
+            index--;
+        }
+        array[index + 1] = r;
+    }
+    public static void Insort(int[] array, int n) {
+        for(int i = 2;i < n; i++) {
+            Insert(array, array[i], i - 1);
+        }
+    }
+    //print
+    public static void print(int[] array) {
+        for(int i = 1;i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    } 
+}
