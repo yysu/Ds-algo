4c2fa9c2696783477910a6a34099ea590d64a65e
YYSU <jim123820@yahoo.com.tw>
2016-12-12 19:54:32 +0800

    hanoi algo修正

--- Hanoi.java
+++ Hanoi.java
@@ -1,13 +1,8 @@
 public class Hanoi {
+
     public static void hanoi(int n, String from, String temp, String to) {
-        if (n == 0) return;
-        hanoi(n-1, from, to, temp);
-        System.out.println("Move disc " + n + " from " + from + " to " + to);
-        hanoi(n-1, temp, from, to);
-    }
-    public static void hanoitest(int n, String from, String temp, String to) {
         if (n == 1) {
-            System.out.println("move disc " + n + " from" + from + "to" + to);
+            System.out.println("move disc " + n + " from " + from + " to " + to);
         } else {
             hanoi(n-1, from, to, temp);
             System.out.println("Move disc " + n + " from " + from + " to " + to);
@@ -16,7 +11,6 @@ public class Hanoi {
     }
     public static void main(String[] args) {
         int n = 3;
-        //hanoi(n, "A", "B", "C");
-        hanoitest(n, "A", "B", "C");
+        hanoi(n, "A", "B", "C");
     }
 } 
