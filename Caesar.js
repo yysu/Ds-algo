a208038d8aaa671a7338d86911f05f4a1a2a706a
YYSU <jim123820@yahoo.com.tw>
2016-12-27 21:10:30 +0800

    政大105數位 資結 凱薩加密

---
+++ Caesar.js
@@ -0,0 +1,22 @@
+//政大105數位 資結考題
+//https://yysu.github.io/jsplayground/
+function GetASCII(Str) {
+	return Str.charCodeAt(0);
+}
+
+function Sub(Str) {
+  if(Str == 32)
+  	return Str;
+  else
+	  return Str-3;
+}
+
+function ToChar(Str) {
+	return String.fromCharCode(Str);
+}
+
+var plainText = "DOT NOT PIG";
+var plainTextArray = plainText.split("");
+var cipherTextArray = plainTextArray.map(GetASCII).map(Sub).map(ToChar);
+var cipherText = cipherTextArray.join("");
+console.log(cipherText);
