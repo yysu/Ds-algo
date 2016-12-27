675b2d8a852030ae53c8e49d4751b4debe401b76
YYSU <jim123820@yahoo.com.tw>
2016-12-27 20:45:13 +0800

    增加十進位轉八進位，運用Stack版本

--- DecimalToBinary.java
+++ DecimalToBinary.java
@@ -1,8 +1,15 @@
+import java.util.*;
+
 public class DecimalToBinary {
     public static void main(String[] argv) {
-        System.out.print(Change(47));
+        //10進位轉2進位
+        //忘了哪間學校
+        System.out.println(decimalToBinary(47));
+        //10進位轉8進位
+        //中央103資結
+        System.out.println(decimalToOctal(68));
     }
-    public static String Change(int number) {
+    public static String decimalToBinary(int number) {
         String result = "";
         while(number != 0) {
             result += number % 2;
@@ -13,5 +20,18 @@ public class DecimalToBinary {
     }
     public static String Reverse(String str) {
         return new StringBuilder(str).reverse().toString();
+    }
+    //運用Stack做反轉[中央103]
+    public static String decimalToOctal(int number) {
+        String result = "";
+        Stack<Integer> S = new Stack<Integer>();
+        while(number != 0) {
+            S.push(number % 8);
+            number /= 8;
+        }
+        while(!S.isEmpty()) {
+            result += S.pop();
+        }
+        return result;
     }
 }
