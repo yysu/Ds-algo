51dab47c957e5cd69c8e1efd0d4f220909241ce3
YYSU <s102213004@mail1.ncnu.edu.tw>
2017-01-21 22:03:35 +0800

    增加政大102

--- BinaryAverageRecursion.java
+++ BinaryAverageRecursion.java
@@ -6,6 +6,10 @@ public class BinaryAverageRecursion {
         //NCCU 100年
         int[] Array = { 1, 2, 3, 4, 5 };
         System.out.println(BinarySum(Array, 0, 5));
+
+        //NCCU 102年
+        int[] Arr = { 1, 2, 3, 4, 5 };
+        System.out.println(BinaryMul(Arr, 0, 5));
     }
     public static double BinaryAvg(double[] A, int start, int n) {
         if(n == 1) {
@@ -51,6 +55,30 @@ public class BinaryAverageRecursion {
                 left = BinarySum(Array, i, n/2);
                 right = BinarySum(Array, i + n/2 , n/2 + 1);
                 return left + right;
+            }
+
+        }
+    }
+    public static int BinaryMul(int[] Array, int i, int n) {
+        if(n == 1) {
+            return Array[i];
+        }
+        else if (n == 2) {
+            return Array[i] * Array[i + 1];
+        }
+        else {
+            int left, right;
+            if( n % 2 == 0) {
+                //陣列是偶數個的處理
+                left = BinaryMul(Array, i, n/2);
+                right = BinaryMul(Array, i + n/2, n/2);
+                return left * right;
+            }
+            else {
+                //陣列是奇數個的處理
+                left = BinaryMul(Array, i, n/2);
+                right = BinaryMul(Array, i + n/2 , n/2 + 1);
+                return left * right;
             }
 
         }
