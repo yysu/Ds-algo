1c1bfb7a00c35614211eded0daa8c6ec11677c13
YYSU <s102213004@mail1.ncnu.edu.tw>
2017-01-26 22:47:58 +0800

    增加Imperative版

--- Basic algo/Binomial.java
+++ Basic algo/Binomial.java
@@ -1,7 +1,23 @@
 public class Binomial {
     public static void main(String[] argv) {
         int m = 4, n = 2;
+        //recursive版
+        System.out.println("C" + m + "取" + n + " : " + C_RE(m, n));
+        //Imperative版
         System.out.println("C" + m + "取" + n + " : " + C(m, n));
+    }
+    public static int C_RE(int m, int n) {
+        int n_fac = 1, m_fac = 1, m_n_fac = 1;
+        for(int i = 1; i <= m; i++) {
+            m_fac *= i;
+        }
+        for(int i = 1; i <= n; i++) {
+            n_fac *= i;
+        }
+        for(int i = 1; i <= m - n; i++) {
+            m_n_fac *= i;
+        }
+        return m_fac / (m_n_fac * n_fac);
     }
     public static int C(int m, int n) {
         if(m == n ||  n == 0)
