da60633b361d0083542f9d2a67ca3de8ec723152
YYSU <s102213004@mail1.ncnu.edu.tw>
2017-07-13 22:57:33 +0800

    coding style fix

--- OO_practice/Object_oriented_practice.java
+++ OO_practice/Object_oriented_practice.java
@@ -5,24 +5,23 @@ import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
 class Alice {
-    public Alice(){
+    public Alice() {
         System.out.println("我是Alice沒內容的建構子");
     }
-    public Alice(String str){
+    public Alice(String str) {
         System.out.println(str);
     }
 }
 class BOB extends Alice {
-    public BOB(){
+    public BOB() {
         System.out.println("我是BOB沒內容的建構子");
     }
-    public BOB(String str){
+    public BOB(String str) {
         System.out.println(str);
     }
 }
 public class Object_oriented_practice {
-    public static void main (String[] args) throws java.lang.Exception
-    {
+    public static void main (String[] args) throws java.lang.Exception {
         new BOB("Hiiiii");
     }
 }
