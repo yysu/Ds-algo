928713c977925cf2e3c899af42bae4f314f21519
jimsu <jim123820@yahoo.com.tw>
2019-03-08 01:03:27 +0800

    Add TreeSet & HashSet

---
+++ Java_usual_function/CollectionObject.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.util.TreeSet;
+import java.util.HashSet;
+
+public class CollectionObject {
+    public static void main(String[] args) {
+        System.out.println("Test HashSet & TreeSet");
+        HashSet<Integer> hashset = new HashSet<>();
+        TreeSet<Integer> treeset = new TreeSet<>();
+        System.out.println("HashSet:");
+        testSet(hashset);
+        System.out.println("");
+        System.out.println("");
+        System.out.println("TreeSet:");
+        testSet(treeset);
+        System.out.println("===================================");
+    }
+
+    public static void testSet(Set<Integer> s) {
+        System.out.println("Hash empty : " + s.isEmpty());
+        s.add(5);
+        s.add(5);
+        s.add(5);
+        s.add(2);
+        s.add(15);
+        s.add(13);
+        s.add(18);
+        s.add(18);
+        s.add(537);
+        s.add(537);
+        System.out.println("Hash size : " + s.size());
+        System.out.println("Hash empty : " + s.isEmpty());
+        System.out.println("Hash : " + s);
+        System.out.println("Hash 是否有 2 : " + s.contains(2));
+        System.out.println("Hash 清除 2");
+        s.remove(2);
+        System.out.println("Hash 是否有 2 : " + s.contains(2));
+        System.out.println("清除Hash");
+        s.clear();
+        System.out.println("Hash : " + s);
+        System.out.println("Hash empty : " + s.isEmpty());
+    }
+}
