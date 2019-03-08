0eefb9b89a1bb1fd9cc99286d7f59fe458a5f5fd
jimsu <jim123820@yahoo.com.tw>
2019-03-08 21:50:05 +0800

    Add Iterator Object

--- Java_usual_function/CollectionObject.java
+++ Java_usual_function/CollectionObject.java
@@ -14,7 +14,19 @@ public class CollectionObject {
         System.out.println("TreeSet:");
         testSet(treeset);
         System.out.println("===================================");
+
+        testIterator(hashset);
+        testIterator(treeset);
     }
+
+    public static void testIterator(Collection c) {
+        Iterator it = c.iterator();
+        while (it.hasNext()) {
+            Object obj = it.next();
+            System.out.print(obj  + " ");
+        }
+        System.out.println();
+    } 
 
     public static void testSet(Set<Integer> s) {
         System.out.println("Hash empty : " + s.isEmpty());
@@ -39,5 +51,16 @@ public class CollectionObject {
         s.clear();
         System.out.println("Hash : " + s);
         System.out.println("Hash empty : " + s.isEmpty());
+        s.add(5);
+        s.add(5);
+        s.add(5);
+        s.add(2);
+        s.add(15);
+        s.add(13);
+        s.add(18);
+        s.add(18);
+        s.add(537);
+        s.add(537);
+        System.out.println("Hash : " + s);
     }
 }
